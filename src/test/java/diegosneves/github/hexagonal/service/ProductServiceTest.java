ad36ca9b52ef0a2435d8c1375514e254fc76c32e
Diego Neves <neves.diegoalex@outlook.com>
2024-02-17 10:32:06 -0300

    feat(M2- Services e Persistências de dados) :white_check_mark: Adicionar `ProductException` aos métodos `ProductContract`
    - Adicionando a cláusula `throws ProductException` aos métodos na interface `ProductContract`.
    - Os métodos implementados na entidade `Product` também refletem essas mudanças respectivamente.
    
    **Adicionar novos testes**
    
    Os seguintes ajustes foram feitos para lidar com os novos cenários:
    
    - Adicionados novos testes ao `ProductServiceTest`.
    - Esses testes servem para validar os cenários em que `ProductException` pode ser lançada a partir da chamada do método de serviço.

--- src/test/java/diegosneves/github/hexagonal/service/ProductServiceTest.java
+++ src/test/java/diegosneves/github/hexagonal/service/ProductServiceTest.java
@@ -3,6 +3,7 @@ package diegosneves.github.hexagonal.service;
 import diegosneves.github.hexagonal.domain.product.entity.Product;
 import diegosneves.github.hexagonal.domain.product.entity.ProductContract;
 import diegosneves.github.hexagonal.domain.product.factory.ProductFactory;
+import diegosneves.github.hexagonal.enums.ProductStatus;
 import diegosneves.github.hexagonal.exceptions.ProductException;
 import diegosneves.github.hexagonal.exceptions.handler.ExceptionHandler;
 import diegosneves.github.hexagonal.repository.ProductPersistenceContract;
@@ -65,6 +66,83 @@ class ProductServiceTest {
         assertNotNull(result);
         assertInstanceOf(ProductException.class, result);
         assertEquals(ExceptionHandler.PRODUCT_ID_SHOULD_NOT_BE_NULL_OR_EMPTY.getMessage(), result.getMessage());
+    }
+
+    @Test
+    void shouldCreateProductWhenValidProductNameAndProductPriceGiven() {
+        String productName = "New Product";
+        Double productPrice = 200.0;
+
+        ProductContract result = this.service.create(productName, productPrice);
+
+        assertNotNull(result);
+        assertEquals(productName, result.getName());
+        assertEquals(productPrice, result.getPrice());
+        assertEquals(ProductStatus.DISABLE, result.getStatus());
+    }
+
+    @Test
+    void shouldThrowProductExceptionWhenAttemptToCreateProductWithEmptyProductName() {
+        String productName = "";
+        Double productPrice = 200.0;
+
+        Exception result = assertThrows(ProductException.class, () -> this.service.create(productName, productPrice));
+
+        assertNotNull(result);
+        assertInstanceOf(ProductException.class, result);
+        assertEquals(ExceptionHandler.PRODUCT_NAME_SHOULD_NOT_BE_NULL_OR_EMPTY.getMessage(), result.getMessage());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenCreatingProductWithNullPrice() {
+        String productName = "New Product";
+
+        Exception result = assertThrows(ProductException.class, () -> this.service.create(productName, null));
+
+        assertNotNull(result);
+        assertInstanceOf(ProductException.class, result);
+        assertEquals(ExceptionHandler.PRICE_LESS_THAN_ZERO.getMessage(), result.getMessage());
+    }
+
+    @Test
+    void shouldEnableProductAndAssertItsStatusIsEnabled(){
+        Product enable = (Product) this.service.enable(this.product);
+
+        ProductStatus status = enable.getStatus();
+
+        assertEquals(ProductStatus.ENABLE, status);
+    }
+
+    @Test
+    void shouldThrowProductExceptionWhenAttemptingToEnableProductWithZeroPrice(){
+        this.product = new Product(UUID, "Produto", 0.0);
+
+        Exception exception = assertThrows(ProductException.class, () -> this.service.enable(this.product));
+
+        assertNotNull(exception);
+        assertInstanceOf(ProductException.class, exception);
+        assertEquals(ExceptionHandler.PRICE_LESS_THAN_ZERO.getMessage(), exception.getMessage());
+    }
+
+    @Test
+    void shouldDisableProductAndAssertItsStatusIsDisable(){
+        this.product = new Product(UUID, "Produto", 0.0);
+
+        Product disabledProduct = (Product) this.service.disable(this.product);
+
+        ProductStatus status = disabledProduct.getStatus();
+
+        assertEquals(ProductStatus.DISABLE, status);
+    }
+
+    @Test
+    void shouldThrowProductExceptionWhenAttemptingToDisableProductPrice(){
+
+        Exception exception = assertThrows(ProductException.class, () -> this.service.disable(this.product));
+
+        assertNotNull(exception);
+        assertInstanceOf(ProductException.class, exception);
+        assertEquals(ExceptionHandler.PRICE_EQUAL_OR_LESS_THAN_ZERO.getMessage(), exception.getMessage());
     }
 
 }
