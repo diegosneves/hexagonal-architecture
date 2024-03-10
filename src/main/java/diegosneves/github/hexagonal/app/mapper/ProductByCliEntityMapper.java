192379c3c9bbd41a4c51683a2ce883c23d284467
Diego Neves <neves.diegoalex@outlook.com>
2024-03-10 19:57:44 -0300

    doc/issues-19(Adicionar Javadoc ao Projeto) :memo: Adicionar comentários Javadoc abrangentes aos mapeadores
    - **Adicionar comentários Javadoc abrangentes aos mapeadores**
    
        Adicionados comentários Javadoc abrangentes a todas as classes e seus métodos dentro do pacote 'mapper' para melhorar a clareza e compreensão de suas funcionalidades.
    
    - **Aumentar a legibilidade**
    
        Isso aumenta a legibilidade e fornece explicações detalhadas sobre os papéis, alcance e responsabilidades de cada classe.
    
    - **Transformando e mapeando objetos de Produto**
    
        Explica como eles transformam e mapeiam objetos de Produto.

--- src/main/java/diegosneves/github/hexagonal/app/mapper/ProductByCliEntityMapper.java
+++ src/main/java/diegosneves/github/hexagonal/app/mapper/ProductByCliEntityMapper.java
@@ -5,6 +5,12 @@ import diegosneves.github.hexagonal.app.domain.product.entity.Product;
 
 import java.lang.reflect.Field;
 
+/**
+ * A classe {@link ProductByCliEntityMapper} implementa a interface {@link MapperStrategy} e é responsável
+ * por mapear um objeto do tipo {@link ProductEntity} para um objeto do tipo {@link Product}.
+ *
+ * @author diegosneves
+ */
 public class ProductByCliEntityMapper implements MapperStrategy<Product, ProductEntity> {
 
     private static final String STATUS_FIELD = "status";
--- src/main/java/diegosneves/github/hexagonal/app/mapper/ProductEntityMapper.java
+++ src/main/java/diegosneves/github/hexagonal/app/mapper/ProductEntityMapper.java
@@ -3,6 +3,14 @@ package diegosneves.github.hexagonal.app.mapper;
 import diegosneves.github.hexagonal.adapters.rest.model.ProductEntity;
 import diegosneves.github.hexagonal.app.domain.product.entity.ProductContract;
 
+/**
+ * A classe {@link ProductEntityMapper} realiza a transformação de um objeto {@link ProductContract} em um objeto {@link ProductEntity}.
+ * Através da implementação da interface {@link MapperStrategy}, esta classe define como essa transformação entre os dois tipos de objeto será executada.
+ * <p>
+ * Este processo é essencial para a correta conversão dos dados do contrato do produto para a sua representação no banco de dados, permitindo a correta persistência e manipulação dos mesmos.
+ *
+ * @author diegosneves
+ */
 public class ProductEntityMapper implements MapperStrategy<ProductEntity, ProductContract> {
 
     @Override
